b354f820897ad2f6fb7f1e209a286e8301c952f9
sangwookWoo <wjddm3@naver.com>
2025-02-18 21:44:09 +0900

    [fix] Add no records case

--- pages/3_♣️head-to-head.py
+++ pages/3_♣️head-to-head.py
@@ -48,47 +48,50 @@ with st.form("Enter Rankings"):
         else:
             data = get_head_to_head_record([player_name1, player_name2])
             df = pd.DataFrame(data)
-            # who is winner?
-            resuilt_df = df.sort_values(by="wins", ascending=False).reset_index()
-            winner = resuilt_df.loc[0, "name"]
-            winner_wins = resuilt_df.loc[0, "wins"]
-
-            loser = resuilt_df.loc[1, "name"]
-            loser_wins = resuilt_df.loc[1, "wins"]
-
-            if winner_wins == loser_wins:
-                st.write("Draw..!")
+            if len(df) <= 1:
+                st.write("There are no records")
             else:
-                col1, col2 = st.columns(2)
-                with col1:
-                    card(
-                        title=f"{winner}(WINNER)",
-                        text=f"{winner_wins} WIN, {loser_wins} LOSE",
-                        key="1",
-                        styles={
-                            "card": {
-                                "margin-top": "10px",
-                                "margin-bottom": "10px",
-                                "margin-left": "1px",
-                                "width": "300px",
-                                "height": "300px",
+                # who is winner?
+                resuilt_df = df.sort_values(by="wins", ascending=False).reset_index()
+                winner = resuilt_df.loc[0, "name"]
+                winner_wins = resuilt_df.loc[0, "wins"]
+
+                loser = resuilt_df.loc[1, "name"]
+                loser_wins = resuilt_df.loc[1, "wins"]
+
+                if winner_wins == loser_wins:
+                    st.write("Draw..!")
+                else:
+                    col1, col2 = st.columns(2)
+                    with col1:
+                        card(
+                            title=f"{winner}(WINNER)",
+                            text=f"{winner_wins} WIN, {loser_wins} LOSE",
+                            key="1",
+                            styles={
+                                "card": {
+                                    "margin-top": "10px",
+                                    "margin-bottom": "10px",
+                                    "margin-left": "1px",
+                                    "width": "300px",
+                                    "height": "300px",
+                                },
                             },
-                        },
-                        image=winner_image,
-                    )
-                with col2:
-                    card(
-                        title=f"{loser}(LOSER)",
-                        text=f"{loser_wins} WIN, {winner_wins} LOSE",
-                        key="2",
-                        styles={
-                            "card": {
-                                "margin-top": "10px",
-                                "margin-bottom": "10px",
-                                "margin-left": "1px",
-                                "width": "220px",
-                                "height": "150px",
+                            image=winner_image,
+                        )
+                    with col2:
+                        card(
+                            title=f"{loser}(LOSER)",
+                            text=f"{loser_wins} WIN, {winner_wins} LOSE",
+                            key="2",
+                            styles={
+                                "card": {
+                                    "margin-top": "10px",
+                                    "margin-bottom": "10px",
+                                    "margin-left": "1px",
+                                    "width": "220px",
+                                    "height": "150px",
+                                },
                             },
-                        },
-                        image=loser_image,
-                    )
+                            image=loser_image,
+                        )
